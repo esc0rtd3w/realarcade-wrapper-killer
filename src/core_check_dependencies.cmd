44540358f313013c1360c35a99633292cf7782c0
Jason Borowski <esc0rtd3w@gmail.com>
2016-05-26 17:07:22 -0400

    Added "core_check_dependencies" Script

---
+++ src/core_check_dependencies.cmd
@@ -0,0 +1,22 @@
+@echo off
+title RealArcade Wrapper Killer v%rawkver%    (.-+'~^-+ Check Dependencies +-^~`+-.)     [...cRypTiCwaRe 2o16...]
+
+
+color 0e
+
+
+cls
+::echo I Am "core_check_dependencies.cmd"
+::echo.
+::echo.
+::pause
+
+
+
+
+goto end
+
+
+
+
+:end
