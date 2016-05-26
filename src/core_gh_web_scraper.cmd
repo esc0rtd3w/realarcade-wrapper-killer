a811f9d3f4d5f692d774b6c0ece098554b58dc50
Jason Borowski <esc0rtd3w@gmail.com>
2016-05-26 17:07:09 -0400

    Added "core_gh_web_scraper" Script

---
+++ src/core_gh_web_scraper.cmd
@@ -0,0 +1,18 @@
+@echo off
+title RealArcade Wrapper Killer v%rawkver%    (.-+'~^-+ GH Web Scraper +-^~`+-.)     [...cRypTiCwaRe 2o16...]
+
+
+:reset
+
+color 0e
+
+
+%runShellWaitTerminate% %ghWebScraper%
+
+
+goto end
+
+
+
+
+:end
