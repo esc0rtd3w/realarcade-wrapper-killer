b87e270e1cc591f09caa4389c5f88b43bf2a694b
Jason Borowski <esc0rtd3w@gmail.com>
2022-12-31 02:59:42 -0500

    Multiple Updates
    - Added logging to text
    - Fixed Path args in setting system paths
    - Removed extra spaces in stubname var
    - Updated XP to use choicexp

--- src/loaders/loaderRGSv2/RGSinst.cmd
+++ src/loaders/loaderRGSv2/RGSinst.cmd
@@ -1,14 +1,22 @@
 @echo off
 
-title RealArcade Wrapper Killer    (.-+'~^-+ RGS Loader v2.1.3 +-^~`+-.)     [...cRypTiCwaRe 2o2o...]
+title RealArcade Wrapper Killer    (.-+'~^-+ RGS Loader v2.1.4 +-^~`+-.)     [...cRypTiCwaRe 2o22...]
 
 color 1f
 
-set PATH=C:\Program Files\unRealArcade\rawk\dump\{sys};C:\Program Files\unRealArcade\tools\unix-utils;%PATH%
+set PATH=%PATH%;"C:\Program Files\unRealArcade\rawk\dump\{sys}";"C:\Program Files\unRealArcade\tools\unix-utils"
 
 ::-----------------------------------------------------------------------------------
 :: Set Windows OS Version
 ::-----------------------------------------------------------------------------------
+set os=XP
+ver | find "5.1" >nul
+if errorlevel 1 set os=VISTA
+::echo os: %os%
+::set
+::pause
+
+
 :: Only checking XP. Anything else will be considered VISTA compatible
 ver | find "5.1"
 if %errorlevel%==0 set os=XP&&goto osCheckDone
@@ -44,13 +52,14 @@ ver | find "10.8"
 if %errorlevel%==0 set os=WIN10&&goto osCheckDone
 ver | find "10.9"
 if %errorlevel%==0 set os=WIN10&&goto osCheckDone
+
+
 :osCheckDone
-
-
 ::-----------------------------------------------------------------------------------
 :: KILL RNARCADE BEFORE PROCEEDING!
 :: added 20131102
 taskkill /f /t /im "RNArcade.exe"
+taskkill /f /t /im "RGSinst.exe"
 ::-----------------------------------------------------------------------------------
 
 
@@ -62,13 +71,18 @@ set gamesroot=C:\My Games
 set downloadroot=C:\My Download Files
 
 set root=%ProgramFiles%\unRealArcade\loaders\loaderRGSv2
+set sys=%ProgramFiles%\unRealArcade\rawk\dump\{sys}
+
 
 set unpacked=0
 
-
 :: This method works, but causes a path with NO SPACES to have a space as the suffix
 :: Everything seems to copy ok still.
-set stubname="%1 %2 %3 %4 %5 %6 %7 %8"
+set stubnameRaw="%1 %2 %3 %4 %5 %6 %7 %8"
+set stubname=%stubnameRaw: =%
+
+::echo stubname: %stubname%
+::pause
 
 set rawkPath=C:\Program Files\unRealArcade
 set rawkExe=rawk.exe
@@ -78,12 +92,16 @@ set rawkCLIPath=C:\Program Files\unRealArcade\rawk\dump\{app}
 set rawkCLIExe=RAUnwrap.cmd
 set rawkCLILaunch="C:\Program Files\unRealArcade\rawk\dump\{app}\*"
 
+set log=%temp%\loaderRGSv2.log
+
 set multirgs=0
 
 
 :start
+echo [INFO] loaderRGSv2 start>>%log%
+::time>>%log%
 ::-----------------------------------------------------------------------------------
-:: Check or a multi RGS installation (added 20131026)
+:: Check for a multi RGS installation (added 20131026)
 ::-----------------------------------------------------------------------------------
 
 if exist "%temp%\ura_multi_install.tmp" set multirgs=1
@@ -115,10 +133,16 @@ cls
 echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
 echo.
 echo.
+echo Source: %stubname%
 echo.
 echo Copying RGS File From Source To Temp....
 echo.
+echo [INFO] Copying RGS file from source to temp START>>%log%
+echo [INFO] RGS Filename: [%stubname%]>>%log%
+
 copy /y %stubname% "%root%\_tmp.rgs"
+echo [INFO] Copying RGS file from source to temp COMPLETE>>%log%
+
 
 
 
@@ -131,18 +155,20 @@ cls
 echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
 echo.
 echo.
+echo Source: %stubname%
 echo.
 echo Extracting RGS File To Temp....
 echo.
 
 if exist "%root%\_tmp.rgs" (
-
+	echo [INFO] RGS Game Installer Copied Successfully>>%log%
 	goto launch
 
 )
 
 
 if not exist "%root%\_tmp.rgs" (
+	echo [ERROR] RGS Game Installer Failed To Copy>>%log%
 
 	color 4f
 
@@ -150,6 +176,7 @@ if not exist "%root%\_tmp.rgs" (
 	echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
 	echo.
 	echo.
+	echo Source: %stubname%
 	echo.
 	echo RGS Game Installer Failed To Copy!
 	echo.
@@ -157,9 +184,11 @@ if not exist "%root%\_tmp.rgs" (
 	echo Force Retry? [Recommended] [Y/N]
 	echo.
 
-	if %os%==XP choice /c:yn /n
-	if not %os%==XP choice /c yn /n
+	if %os%==XP choicexp /c:yn /n
+	if %os%==VISTA choice /c yn /n
 	if errorlevel 2 goto end
+	
+	pause
 
 	goto skiptmp
 
@@ -182,6 +211,7 @@ cls
 echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
 echo.
 echo.
+echo Source: %stubname%
 echo.
 echo Extracting RGS File To Temp....
 echo.
@@ -202,7 +232,9 @@ echo.
 echo NOTE: If the file is corrupted, neither one of the loaders will work!
 echo.
 
+echo [INFO] Launching Game Installer START>>%log%
 "%root%\RGSInst.exe"
+echo [INFO] Closing Game Installer COMPLETE>>%log%
 
 cocolor 1f
 
@@ -215,6 +247,7 @@ goto chkxerr
 ::-----------------------------------------------------------------------------------
 
 if not exist "%root%\_tmp\*.exe" (
+	echo [ERROR] Game EXE Not Exist>>%log%
 
 	color 4f
 
@@ -222,6 +255,7 @@ if not exist "%root%\_tmp\*.exe" (
 	echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
 	echo.
 	echo.
+	echo Source: "%root%\_tmp"
 	echo.
 	echo Corrupted Extraction Folder Detected!
 	echo.
@@ -229,9 +263,11 @@ if not exist "%root%\_tmp\*.exe" (
 	echo Force v1.x Loader? [Recommended] [Y/N]
 	echo.
 
-	if %os%==XP choice /c:yn /n
-	if not %os%==XP choice /c yn /n
-	if errorlevel 2 exit
+	if %os%==XP choicexp /c:yn /n
+	if %os%==VISTA choice /c yn /n
+	if errorlevel 2 goto end
+
+	echo [INFO] Fallback to v1 loader START>>%log%
 
 
 	cls
@@ -269,9 +305,9 @@ if not exist "%root%\_tmp\*.exe" (
 
 	regedit /s "%ProgramFiles%\unRealArcade\loaderRGSv2\setRGS.reg"
 
+	echo [INFO] Fallback to v1 loader COMPLETE>>%log%
 
-
-	exit
+	end
 
 )
 
@@ -285,15 +321,16 @@ goto chkextr
 ::-----------------------------------------------------------------------------------
 
 if exist "%root%\_tmp" (
+	echo [INFO] Game Extraction Folder Detected>>%log%
 
 	set unpacked=1
 	goto getini
 
 )
 
-goto end
 
 if not exist "%root%\_tmp" (
+	echo [ERROR] Game Extraction Folder Not Detected>>%log%
 
 	color 4f
 
@@ -301,6 +338,7 @@ if not exist "%root%\_tmp" (
 	echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
 	echo.
 	echo.
+	echo Source: "%root%\_tmp"
 	echo.
 	echo No previous extraction found!
 	echo.
@@ -308,8 +346,8 @@ if not exist "%root%\_tmp" (
 	echo Force Retry? [Recommended] [Y/N]
 	echo.
 
-	if %os%==XP choice /c:yn /n
-	if not %os%==XP choice /c yn /n
+	if %os%==XP choicexp /c:yn /n
+	if %os%==VISTA choice /c yn /n
 	if errorlevel 2 goto end
 
 	goto deltmp
@@ -317,11 +355,6 @@ if not exist "%root%\_tmp" (
 )
 
 goto end
-
-
-
-
-:end
 
 
 :getini
@@ -336,21 +369,37 @@ echo.
 
 wait 2
 
+echo [INFO] Extracting Game Name From launch.ini START>>%log%
+
 :: Getting Game Name from launch.ini
 inifile "%root%\_tmp\launch.ini" [Main] GameName
 
 for /f "delims=" %%a in ('inifile.exe "%root%\_tmp\launch.ini" [Main] GameName') do %%a
 
+echo [INFO] Extracting Game Name From launch.ini COMPLETE>>%log%
+
+echo [INFO] Setting GameName for games that have issues getting correct value START>>%log%
+
+if exist "%root%\_tmp\gia_crimeandpunishment_r1a.exe" set GameName=Crime and Punishment - Who Framed Raskolnikov
 if exist "%root%\_tmp\ds.exe" set GameName=DemonStar
 if exist "%root%\_tmp\windm.exe" set GameName=Diamond Mine
+if exist "%root%\_tmp\escapefromlostisland_r1a.exe" set GameName=Escape From Lost Island
+if exist "%root%\_tmp\fortunetiles_r1a.exe" set GameName=Fortune Tiles Gold
+if exist "%root%\_tmp\jetsetter_r1a.exe" set GameName=Jet Setter
+if exist "%root%\_tmp\earth day_r1a.exe" set GameName=National Geographic Earth Day Pack
 if exist "%root%\_tmp\p3.exe" set GameName=Passage 3
 if exist "%root%\_tmp\p3tool.exe" set GameName=Passage 3
 if exist "%root%\_tmp\id-ossmallball.exe" set GameName=SmallBall Baseball
+if exist "%root%\_tmp\wobblybobbly_r1a.exe" set GameName=Wobbly Bobbly
+
+echo [INFO] Setting GameName for games that have issues getting correct value COMPLETE>>%log%
+echo [INFO] Extracted Game Name: "%GameName%">>%log%
 
 
-
+echo [INFO] RGS Unpack START>>%log%
 :: Launch Wrapper Killer if successful extraction (added 20131019)
 if %unpacked%==1 (
+	echo [INFO] RGS Unpack SUCCESS>>%log%
 
 	color 2f
 
@@ -394,16 +443,33 @@ if %unpacked%==1 (
 )
 
 if %unpacked%==0 (
-
-	exit
+	echo [ERROR] RGS Unpack FAILED>>%log%
+	color 4f
+	cls
+	echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
+	echo.
+	echo.
+	echo The unpacking FAILED!
+	echo.
+	echo.
+	echo Source: "%root%\_tmp"
+	echo.
+	echo No "_tmp"folder detected.
+	echo.
+	pause
+	goto end
 
 )
 
 :optMenu
+echo [INFO] Launch Wrapper Killer START>>%log%
+echo [INFO] Ready to unpack the Demo Protection on this game>>%log%
 
 cls
-echo Ready to unpack the Demo Protection on this game!
+echo DO NOT CLOSE THIS WINDOW!! IT WILL CLOSE WHEN FINISHED!!
 echo.
+echo.
+echo Ready to unpack the Demo Protection on this game!
 echo.
 echo.
 echo.
@@ -419,14 +485,19 @@ echo This option will disappear in 5 seconds and load default settings....
 echo.
 echo.
 
-if %os%==XP choice /c:dgx /t:d,5 /n
-if not %os%==XP choice /c dgx /d d /n /t 5
+if %os%==XP choicexp /c:dgx /t:d,5 /n
+if %os%==VISTA choice /c dgx /d d /n /t 5
+
+::echo %errorlevel%
+::pause
+
 if errorlevel 3 goto end
 if errorlevel 2 goto runGUI
 if errorlevel 1 goto runCLI
 
 
 :runCLI
+echo [INFO] Run CLI Wrapper Killer START>>%log%
 ::echo runCLI
 ::pause
 
@@ -436,20 +507,31 @@ set cliActive=1
 copy %rawkCLILaunch% "%gamesroot%\%GameName%"
 start "" /d "%gamesroot%\%GameName%" "%rawkCLIExe%"
 
+echo [INFO] CLI Params - %rawkCLILaunch% "%gamesroot%\%GameName%">>%log%
+echo [INFO] Run CLI Wrapper Killer END>>%log%
+
 goto end
 
 
 :runGUI
+echo [INFO] Run GUI Wrapper Killer START>>%log%
 ::echo runGUI
 ::pause
 
 ::copy %rawkCLILaunch% "%gamesroot%\%GameName%"
 start "" /d "%rawkPath%" "%rawkExe%"
 
+echo [INFO] Run GUI Wrapper Killer END>>%log%
+echo [INFO] Launch Wrapper Killer END>>%log%
+
 goto end
 
 
 :end
+echo [INFO] loaderRGSv2 end>>%log%
+::time>>%log%
+echo.>>%log%
+echo.>>%log%
 
 exit
 
